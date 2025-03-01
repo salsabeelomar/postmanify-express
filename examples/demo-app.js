4321bd5d6a96b9c260265f2b4b4be69d2c3da27c
basma skaik <basmahskaik@gmail.com>
2025-03-01 15:27:38 +0200

    update gitignore file

--- examples/demo-app.js
+++ examples/demo-app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.get("/home", (req, res) => res.status(200).send("Hello World"));
 
@@ -75,7 +75,9 @@ app.put("/users/:id", (req, res) => {
 // Start server only if not in test environment
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`server is running on http://localhost:${PORT}`)
+  );
 }
 
 module.exports = app;
